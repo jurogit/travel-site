d50e33977dbd92909b9990599a06ead5a2fca5e0
rohasz <robertjuhasz@t-online.hu>
2017-06-16 09:14:43 +0200

    Finished mobile-nav section.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,15 +1,4 @@
-var $ = require("jquery");
-import Person from "./modules/Person";
+/* jshint esversion: 6 */
+import MobileMenu from "./modules/MobileMenu";
 
-class Adult extends Person {
-	payTaxes() {
-		console.log(this.name + " now owes $0 in taxes.");
-	}
-}
-
-var john = new Person("John Doe", "blue");
-john.greet(); // output: Hello, my name is John Doe and my favorite color is blue.
-
-var jane = new Adult("Jane Smith", "orange");
-jane.greet(); // output: Hello, my name is Jane Smith and my favorite color is green.
-jane.payTaxes();
+var mobileMenu = new MobileMenu();
